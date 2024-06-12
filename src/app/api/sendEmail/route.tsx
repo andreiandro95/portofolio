b6f2a95c4fadc83c62085955f6c7ca054e5f8fc2
Andronachi Andrei <andrei.andronachi95@gmail.com>
2024-06-12 09:40:55 +0300

    update portofolio

--- src/app/api/sendEmail/route.tsx
+++ src/app/api/sendEmail/route.tsx
@@ -16,6 +16,18 @@ export async function POST(req: NextRequest) {
       },
     });
 
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error: Error | null, success: any) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
+
     const mailOptions = {
       from: content.email,
       to: "andrei.andronachi95@gmail.com",
@@ -27,18 +39,20 @@ export async function POST(req: NextRequest) {
         `,
     };
 
-    const info = await transporter.sendMail(
-      mailOptions,
-      (error: Error | null, info: { response: string }) => {
-        if (error) {
-          console.error("❌ Error:", error.message);
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err: Error | null, info: string) => {
+        if (err) {
+          console.error(err);
+          reject(err);
         } else {
-          console.log("✅ Email sent:", info.response);
+          console.log(info);
+          resolve(info);
         }
-      }
-    );
+      });
+    });
 
-    return NextResponse.json({ message: info }, { status: 200 });
+    return NextResponse.json({ message: "Email sent" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 400 });
   }

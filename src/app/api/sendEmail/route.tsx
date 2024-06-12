1e4d2f68d6d58c1fd23e2bb65e4424a6ea97ed04
Andronachi Andrei <andrei.andronachi95@gmail.com>
2024-06-12 09:15:20 +0300

    update portofolio

--- src/app/api/sendEmail/route.tsx
+++ src/app/api/sendEmail/route.tsx
@@ -6,16 +6,10 @@ export async function POST(req: NextRequest) {
     const content = await req.json();
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      service: "Gmail",
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
-      },
-      tls: {
-        rejectUnauthorized: false,
       },
     });
 
